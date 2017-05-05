5b80a9bdc0713622fcd3c9e885034a6474f9c025
Isabella Abatgis <abatgis@gmail.com>
2017-05-05 10:07:41 -0700

    separation of front and back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,28 @@
-// // Back End Logic //
-//
-//
-//   for (var i=1; i <= userInput; i++)
-//   {
-//       if (i % 15 == 0)
-//           $("#output").append("<li>" + "ping-pong" + "</li>");
-//       else if (i % 3 == 0)
-//           $("#output").append("<li>" + "ping" + "</li>");
-//       else if (i % 5 == 0)
-//           $("#output").append("<li>" + "pong" + "</li>");
-//       else
-//           $("#output").append("<li>" + i + "</li>");
-//   }
+// Back End Logic //
+
+function pingPong(userInput) {
+  var solution = [];
+  for (var index = 1; index <= userInput; index += 1)
+  {
+    if (index % 15 == 0) {
+      solution.push("<li>" + "ping-pong" + "</li>");
+    } else if (index % 3 == 0) {
+      solution.push("<li>" + "ping" + "</li>");
+    } else if (index % 5 == 0) {
+      solution.push("<li>" + "pong" + "</li>");
+    } else
+      solution.push("<li>" + index + "</li>");
+  };
+  return solution;
+}
+
 
 // Front End Logic //
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    ;debugger
     var userInput = parseInt($("#number").val());
-    for (var index = 1; index <= userInput; index += 1)
-    {
-        if (index % 15 == 0) {
-            $("#output").append("<li>" + "ping-pong" + "</li>");
-        } else if (index % 3 == 0) {
-            $("#output").append("<li>" + "ping" + "</li>");
-        } else if (index % 5 == 0) {
-            $("#output").append("<li>" + "pong" + "</li>");
-        } else
-            $("#output").append("<li>" + index + "</li>");
-        }
+    var result = pingPong(userInput);
+    $("#output").append(result);
   });
 })
